a1bfdef130bda0e5121c2441ef5a5834111ef15a
Fernando <fernando.herrera85@gmail.com>
2022-12-19 12:52:30 -0500

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -14,6 +14,6 @@ async function bootstrap() {
 
   await app.listen(3000);
   
-  console.log('App running on port::', 3000 );
+  console.log('Aplicación corriendo en puerto: ', 3000 );
 }
 bootstrap();
